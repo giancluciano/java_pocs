cea402986672850bf436e23dde44c911050761e5
gian <giancluciano@gmail.com>
2026-05-07 18:13:13 -0300

    improve comsumer

--- pure_kafka_poc/consumer_app/consumer-app/src/test/java/com/example/AppTest.java
+++ pure_kafka_poc/consumer_app/consumer-app/src/test/java/com/example/AppTest.java
@@ -1,17 +1,11 @@
 package com.example;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
+public class AppTest
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
     public void shouldAnswerWithTrue()
     {
